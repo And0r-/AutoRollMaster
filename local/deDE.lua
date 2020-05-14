d3ff6b8255fc02617a30333d1f87afbbf922280f
And0r <ahabegger@gmail.com>
2020-05-14 02:53:57 +0200

    next build test for translations...

--- local/deDE.lua
+++ local/deDE.lua
@@ -1,6 +1,6 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AutoRoll", "deDE")
 
---@localization(locale="deDE", format="lua_additive_table", namespace="AutoRoll")@
+--@localization(locale="deDE", format="lua_additive_table")@
 
 --@do-not-package@
 L["ein test"] = "de local test ok"
