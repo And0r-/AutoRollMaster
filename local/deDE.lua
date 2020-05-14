ee61370429cb8c0ade6798a1c4c22a82139cf8df
And0r <ahabegger@gmail.com>
2020-05-14 04:35:26 +0200

    curse localisation do not work :(

--- local/deDE.lua
+++ local/deDE.lua
@@ -1,11 +1,9 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AutoRoll", "deDE")
 
---@localization(locale="deDE", format="lua_additive_table")@
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@
 
 --@do-not-package@
 L["ein test"] = "de local test ok"
 L["zweiter test"] = "de local test2 ok"
-
-
 
 --@end-do-not-package@
