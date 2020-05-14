9e6ae954d35a627aae3937c48caf7176bf6fe0ea
And0r <ahabegger@gmail.com>
2020-05-14 02:44:55 +0200

    hmmm i has the false namespace, and see no change. maybe i has to crate a new package to see a change

--- local/deDE.lua
+++ local/deDE.lua
@@ -1,9 +1,10 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AutoRoll", "deDE")
 
---@localization(locale="deDE", format="lua_additive_table")@
+--@localization(locale="deDE", format="lua_additive_table", namespace="AutoRoll")@
 
 --@do-not-package@
 L["ein test"] = "de local test ok"
+L["zweiter test"] = "de local test2 ok"
 
 
 
